24069ff0790bbf04eb387e275660972fb1bf6232
Ananya Anjali <111357426+Nishuananya19@users.noreply.github.com>
2023-09-07 00:53:22 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".textt",  {
+    strings:["Freelancer","Web Developer","Frontend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
